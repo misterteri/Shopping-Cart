587d211878b45163685f1a4df9eb8e549bf8f5a1
Marcelino Gilbert Tagore <marcelinogilbert26@gmail.com>
2023-04-20 14:56:20 +0800

    Routing for each page completed

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/navbar";
+import { Shop } from "./pages/shop/shop";
+import { Cart } from "./pages/cart/cart";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        {/* Navbar is put in between Router and 
+        Routes so it will always appear on everypage */}
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
